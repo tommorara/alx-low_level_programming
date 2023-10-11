28199061178c1414ac721efc6d5a00d85973eef7
Tom Morara <morara1920@gmail.com>
2023-10-12 01:52:20 +0300

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+
+/*Define the print_alphabet function*/
+	void print_alphabet(void)
+	{
+	char letter;
+	for(letter >= 'a'; letter <= 'z'; letter++);
+	{
+	_putchar(letter);
+	_putchar('/n);
+	return (0);
+	}
+	}
