61b0e56f7b78422f317d3155b693d322fd7ce608
Tom Morara <morara1920@gmail.com>
2023-10-12 11:51:31 +0300

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**
+ * _islower - check if char is lowercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
+ */
+	int _islower(int c)
+	{
+	if (c >= 'a' && c <= 'z')
+	{
+	returns(1);
+	}
+	else
+	{
+	return (0);
+	}
+	}
