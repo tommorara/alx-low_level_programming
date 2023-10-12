a25e2d5dfc7ef6ccfa31021719de34c7b4b55149
Tom Morara <morara1920@gmail.com>
2023-10-12 12:10:49 +0300

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,22 +4,21 @@
 * @n: for checking int
 * Return: 1 and prints + if n is greater than zero
 */
-	int print_sign(int n)
-
-	{
+int print_sign(int n)
+{
 	if (n > 0)
 	{
-	_putchar('+');
-	return (1);
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
-	}
-	_putchar(48);
-	return (0);
+	{
+		_putchar(48);
+		return (0);
 	}
 	else if (n < 0)
 	{
-	_putchar('-');
+		_putchar('-');
 	}
-	return (-1);
-	}
+		return (-1);
+}
