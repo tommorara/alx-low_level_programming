bb66f4d676f60ce43de020e11a4098046b57231c
Tom Morara <morara1920@gmail.com>
2023-10-12 03:46:16 +0300

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - prints the the coresponding sign of a number
+* @n: for checking int
+* Return: 1 and prints + if n is greater than zero
+*/
+	int print_sign(int n)
+
+	{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n == 0)
+	}
+	_putchar(48);
+	return (0);
+	}
+	else if (n < 0)
+	{
+	_putchar('-');
+	}
+	return (-1);
+	}
