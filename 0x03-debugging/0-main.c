4f51efb0ad4f9b22c5dcd30dd6dfd3ec80b537e2
Tom Morara <morara1920@gmail.com>
2023-10-14 23:38:15 +0300

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - The function tests for positive and negative numbers
+* Return: 0
+*/
+	int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
