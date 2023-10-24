49d89a08debd33b9fa3bb036edf03233dddbf426
Tom Morara <morara1920@gmail.com>
2023-10-24 22:30:30 +0300

    revised

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,14 +7,17 @@
  * Return: Always 0.
  */
 
-void print_chessboard(char (*a)[8]) {
-    int i, j;
+void print_chessboard(char (*a)[8])
 
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            putchar(a[i][j]);
-        }
-        putchar('\n');
-    }
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+	for (j = 0; j < 8; j++)
+	{
+		putchar(a[i][j]);
+	}
+	putchar('\n');
+	}
 }
-
