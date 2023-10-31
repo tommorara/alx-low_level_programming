4b250b3893b1387d8c8afec164058031507ca4dd
Tom Morara <morara1920@gmail.com>
2023-10-31 18:19:57 +0300

    If you even dream of beating me you did better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* alloc_grid - returns a pointer to a 2 dimensional array of integers.
+* @width: the with of the array
+* @height: the height of the array
+* Return: a pointer to a 2 dimension array or null
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **gridArr, i, j;
+	int len = width * height;
+
+	if (len <= 0)
+		return (NULL);
+	gridArr = (int **)malloc(height * sizeof(int *));
+	if (gridArr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		gridArr[i] = (int *)malloc(width * sizeof(int));
+		if (gridArr[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(gridArr[i]);
+			free(gridArr);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			gridArr[i][j] = 0;
+		}
+	}
+	return (gridArr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,9 +1,11 @@
-#ifndef MAIN_H 
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *str_concat(char *s1, char *s2);
-#endif
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
+#endif
