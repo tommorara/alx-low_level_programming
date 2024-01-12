51fd7b1fdb5628af3d1ac5d7e207d75f69888d1a
Tom Morara <morara1920@gmail.com>
2024-01-12 04:34:39 +0300

    all

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * struct listint_s-singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+size_t print_dlistint(const dlistint_t *h);
+
+
+
+
+
+#endif /* LISTS_H */
