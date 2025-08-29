413c0fc573928e83d7ae4cab3576ff38478fe570
Tom Morara <morara1920@gmail.com>
2025-08-29 21:12:47 +0300

    290825

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: This program prints the message
+ * "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line, using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
